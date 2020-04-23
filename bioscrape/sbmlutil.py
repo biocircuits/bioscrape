ee799b2ebc2825506a9d657ccfbba2c55a17168a
ayush9pandey <ayush.9.pandey@gmail.com>
2020-04-22 21:58:58 -0700

    removed duplicate function getAllIds

--- bioscrape/sbmlutil.py
+++ bioscrape/sbmlutil.py
@@ -806,21 +806,6 @@ class SetIdFromNames(libsbml.IdentifierTransformer):
     #  #
 
 
-#  # Returns a list of all ids from the given list of elements
-#  #
-def getAllIds(allElements):
-    result = []
-    if (allElements == None or allElements.getSize() == 0):
-        return result
-
-    for i in range(0, allElements.getSize()):
-        current = allElements.get(i)
-        if (current.isSetId() \
-           and current.getTypeCode() != libsbml.SBML_LOCAL_PARAMETER):
-            result.append(current.getId())
-    return result
-
-
 def getSpeciesByName(model, name, compartment=''):
     '''
     Returns a list of species in the Model with the given name
