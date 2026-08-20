4683e57286a457e2f6e6c0f9e6933d6583f57faa
ayush9pandey <ayush.9.pandey@gmail.com>
2026-08-19 20:40:55 -0700

    add dynamic versioning so that github tags are used for building the package. remove python 3.8 because it is deprecated. add setuptoosl so it builds distributions for windows and macos as well. update pypi release action accordingly.

--- bioscrape/__init__.py
+++ bioscrape/__init__.py
@@ -1,6 +1,8 @@
+from importlib.metadata import version 
 import bioscrape.random
 import bioscrape.types
 import bioscrape.simulator
 import bioscrape.inference
 
 bioscrape.random.py_seed_random()
+__version__ = version("Bioscrape")
